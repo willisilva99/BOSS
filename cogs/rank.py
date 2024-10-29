8c84bff12d9b6d05fda841fcd30d6c04bea4666b
willisilva99 <126760543+willisilva99@users.noreply.github.com>
2024-10-29 16:21:36 -0300

    Update rank.py

--- cogs/rank.py
+++ cogs/rank.py
@@ -38,7 +38,7 @@ class RankCog(commands.Cog):
         self.channel_id = 1186636197934661632
 
     async def cog_load(self):
-        # Inicializa a conexão com o banco de dados
+        """Inicializa a conexão com o banco de dados e carrega rankings."""
         self.db_pool = await asyncpg.create_pool(dsn=os.getenv("DATABASE_URL"))
         await self.create_tables()  # Cria tabelas no banco de dados
         await self.load_rankings()  # Carrega rankings do banco de dados
@@ -78,24 +78,24 @@ class RankCog(commands.Cog):
         """Registra o dano causado por um usuário e atualiza o banco de dados."""
         self.damage_rank[user_id] += damage
         asyncio.create_task(self.update_database(user_id, "total_damage", damage))
-        print(f"Registro de dano: {user_id} causou {damage} de dano.")  # Adicionei um log para verificação
+        print(f"Registro de dano: {user_id} causou {damage} de dano.")
 
     def record_kill(self, user_id):
         """Registra uma kill no boss por um usuário e atualiza o banco de dados."""
         self.kill_rank[user_id] += 1
         asyncio.create_task(self.update_database(user_id, "kills", 1))
-        print(f"Registro de kill: {user_id} realizou uma kill.")  # Adicionei um log para verificação
+        print(f"Registro de kill: {user_id} realizou uma kill.")
 
     def record_sniper(self, user_id):
         """Registra uma sniper ganha por um usuário e atualiza o banco de dados."""
         self.sniper_rank[user_id] += 1
         asyncio.create_task(self.update_database(user_id, "snipers", 1))
-        print(f"Registro de sniper: {user_id} ganhou uma sniper.")  # Adicionei um log para verificação
+        print(f"Registro de sniper: {user_id} ganhou uma sniper.")
 
     @commands.Cog.listener()
     async def on_ready(self):
-        # Aguardar para garantir que o bot está totalmente conectado
-        await asyncio.sleep(5)
+        """Inicia as tarefas de ranking e atualização de cargos."""
+        await asyncio.sleep(5)  # Aguardar para garantir que o bot está totalmente conectado
         channel = self.bot.get_channel(self.channel_id)
         if channel:
             print(f"Canal de rank encontrado: {channel.name} (ID: {channel.id})")
