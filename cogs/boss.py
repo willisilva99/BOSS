a2f05dadec951120a6f78f4658fa4536991eb664
willisilva99 <126760543+willisilva99@users.noreply.github.com>
2024-10-28 21:03:55 -0300

    Update boss.py

--- cogs/boss.py
+++ cogs/boss.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands, tasks
 import random
 import time
-import asyncio  # Adicionando a importação do asyncio
+import asyncio
 
 class BossCog(commands.Cog):
     def __init__(self, bot):
